e31087668ebfe0b3de17d089b74fd299d5217fe8
Thomas O'Donnell <andy.tom@gmail.com>
2013-02-02 19:30:51 +0000

    Adding some changes to the docstrings for forms.

--- base64r/forms.py
+++ base64r/forms.py
@@ -2,12 +2,11 @@
     base64r forms
     =============
 
-    Forms for base64r
+    Forms for base64r.
 
     :copyright: (c) 2013 by Thomas O'Donnell.
     :license: BSD, see LICENSE for more details.
     :version: 0.1
-
 """
 
 from flask_wtf import Form, TextField, Required, TextAreaField
