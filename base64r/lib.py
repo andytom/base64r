99ca32778859d77948c9f56600f74d229328f8d0
Thomas O'Donnell <andy.tom@gmail.com>
2013-02-02 19:27:27 +0000

    Correcting to the docstrings.

--- base64r/lib.py
+++ base64r/lib.py
@@ -1,23 +1,35 @@
+"""
+    base64r libs
+    ============
+
+    A libary containing helper functions for dealing with base64 decoding of
+    files.
+
+    :copyright: (c) 2013 by Thomas O'Donnell.
+    :license: BSD, see LICENSE for more details.
+    :version: 0.1
+"""
 import os
 import uuid
 import base64
 
 #------------------------------------------------------------------------------#
-# Helper Functions
+# Files
 #------------------------------------------------------------------------------#
 def create_filename( extention ):
-    """Create a random filename with the passed extention."""
+    """Create a unique filename with the passed extention."""
     random_string = str( uuid.uuid4() )
     return '.'.join( [random_string, extention] )
 
 def ensure_dir( filename) :
-    """Make sure that the passed directory exists, create it if it does not."""
+    """Make sure that the passed directory containing the passed filename
+    exists, create it if it does not."""
     dir = os.path.dirname(filename)
     if not os.path.exists( dir ):
         os.makedirs( dir )
 
 def write_file( bin_data, filename, directory ):
-    """Save the passed file as filename in the OUTPUT_FOLDER."""
+    """Save the passed file as filename in the passed directory."""
     full_filename = os.path.join( directory, filename )
     ensure_dir( full_filename )
 
@@ -29,7 +41,7 @@ def write_file( bin_data, filename, directory ):
 
 def base64_decode( base64_string, extention, directory ):
     """Decode the Base64 sting and write it to a file random named file with
-       the passed extention.
+       the passed extention in the passed directory.
     """
     bin_data = base64.b64decode( base64_string )
     filename = create_filename( extention )
